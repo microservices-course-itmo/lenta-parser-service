31208c60bfbbfd3d97dd511eb7bba3e0ed088cd5
butenko.or <butenko.or@esphere.ru>
2020-09-29 18:23:32 +0300

    Создан класс Parser, который собирает текст из html-страницы по тегам Добавлен выввод в строку

--- src/main/java/com/wine/to/up/demo/service/parser/Parser.java
+++ src/main/java/com/wine/to/up/demo/service/parser/Parser.java
@@ -2,6 +2,9 @@ package com.wine.to.up.demo.service.parser;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.Jsoup;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Parser {
@@ -10,17 +13,24 @@ public class Parser {
     }
 
     public void parseTitle() throws IOException {
+        File file = new File("file.txt");
+        if(file.createNewFile()){
+            System.out.println("file.txt файл создан в корневой директории проекта");
+        }else System.out.println("file.txt файл уже существует в корневой директории проекта");
+        FileWriter writer = new FileWriter("file.txt", true);
+
         Document doc = Jsoup.connect("https://lenta.com/catalog/alkogolnye-napitki/vino/").get();
         String title = doc.title();
+        writer.write(title);
         System.out.println("Title : " + title);
-    }
 
-    public void parseText() throws IOException {
         Document doc1 = Jsoup.connect("https://lenta.com/product/vino-abrau-kupazh-tjmnyjj-krasnoe-suh-rossiya-075l-379932/").get();
         String text = doc1.text();
         String[] words = text.split(" ");
         for (String word : words) {
+            writer.write(word + '\n');
             System.out.println(word);
         }
+
     }
 }
