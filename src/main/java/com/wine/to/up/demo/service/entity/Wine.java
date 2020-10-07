5653b894352dd99b12b77103c246cdaa48b2d121
viprikz <viprikz@mail.ru>
2020-10-07 17:39:49 +0300

    Рефакторинг кода

--- src/main/java/com/wine/to/up/demo/service/entity/Wine.java
+++ src/main/java/com/wine/to/up/demo/service/entity/Wine.java
@@ -1,45 +1,75 @@
-package com.wine.to.up.demo.service.entity;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
-import java.math.BigDecimal;
-
-@Entity
-@Getter
-@Setter
-@Table (name = "wines")
-public class Wine {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @Column (name = "name")
-    private String name;
-
-    @Column (name = "type")
-    private String type;
-
-    @Column (name = "country")
-    private String country;
-
-    @Column (name = "rating")
-    private int rating;
-
-    @Column (name = "valume")
-    private float volume;
-
-    @Column (name = "normal_price")
-    private BigDecimal normalPrice;
-
-    @Column (name = "discount_price")
-    private BigDecimal discountPrice;
-
-    @Column (name = "discount_percentage")
-    private int discountPercentage;
-
-    @Column (name = "image")
-    private String image;
-}
+//package com.wine.to.up.demo.service.entity;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import javax.persistence.*;
+//import java.math.BigDecimal;
+//
+///**
+// * Позиция вина на сайте Ленты
+// */
+//@Entity
+//@Getter
+//@Setter
+//@Table (name = "wines")
+//public class Wine {
+//
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//
+//    /**
+//     * Название вина
+//     */
+//    @Column (name = "name")
+//    private String name;
+//
+//    /**
+//     * Тип вина
+//     */
+//    @Column (name = "type")
+//    private String type;
+//
+//    /**
+//     * Страна - производитель вина
+//     */
+//    @Column (name = "country")
+//    private String country;
+//
+//    /**
+//     * Рейтинг вина
+//     */
+//    @Column (name = "rating")
+//    private int rating;
+//
+//    /**
+//     * Объем бутылки вина
+//     */
+//    @Column (name = "valume")
+//    private float volume;
+//
+//    /**
+//     * Стандартная цена вина
+//     */
+//    @Column (name = "normal_price")
+//    private BigDecimal normalPrice;
+//
+//    /**
+//     * Цена вина по скидке
+//     */
+//    @Column (name = "discount_price")
+//    private BigDecimal discountPrice;
+//
+//    /**
+//     * Размер скидки в процентах
+//     */
+//    @Column (name = "discount_percentage")
+//    private int discountPercentage;
+//
+//    /**
+//     * Ссылка на изображение вина
+//     */
+//    @Column (name = "image")
+//    private String image;
+//}
