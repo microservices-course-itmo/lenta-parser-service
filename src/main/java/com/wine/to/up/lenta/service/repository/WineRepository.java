00a2c50451e352f30bb155998d84c8857a9c6570
viprikz <viprikz@mail.ru>
2020-10-14 18:22:07 +0300

    Рефакторинг

---
+++ src/main/java/com/wine/to/up/lenta/service/repository/WineRepository.java
@@ -0,0 +1,15 @@
+package com.wine.to.up.lenta.service.repository;//package com.wine.to.up.demo.service.repository;
+//
+///**
+// * Репозиторий для таблицы wines
+// */
+//public interface WineRepository {
+//
+//    /**
+//     * Метод, который возвращает сущность "Wine" по ее id в базе данных
+//     *
+//     * @param id - идентификационный номер записи в базе данных
+//     * @return - сущность "Wine"
+//     */
+//    public Wine findById(int id);
+//}
