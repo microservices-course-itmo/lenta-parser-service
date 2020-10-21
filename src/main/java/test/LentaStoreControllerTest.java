1819a83f4f38b9bb3722e2b9e093cd10cdc1a086
viprikz <viprikz@mail.ru>
2020-10-21 18:13:22 +0300

    - Adding Tests

---
+++ src/main/java/test/LentaStoreControllerTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+public class LentaStoreControllerTest extends AbstractTest {
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+    //GET API test
+    @Test
+    public void getProductsList() throws Exception {
+        String uri = "/lenta/parser";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        content = content.substring(1, content.length());
+        JSONObject productlist = new JSONObject(content);
+        assertTrue(productlist.length()> 0);
+    }
+}
