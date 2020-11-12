a90f566717e6d52be6ee4763c9fc7fa7dccd3887
kosch71 <kostya.chikishev@inbox.ru>
2020-11-12 10:06:46 +0300

    ## Minor changes - Refactoring LentaWineParserServiceImplHelperTest

--- src/test/java/com/wine/to/up/lenta/service/helpers/LentaWineParserServiceImplHelperTest.java
+++ src/test/java/com/wine/to/up/lenta/service/helpers/LentaWineParserServiceImplHelperTest.java
@@ -14,54 +14,41 @@ import static org.junit.Assert.assertEquals;
 @NoArgsConstructor
 public class LentaWineParserServiceImplHelperTest {
     @Test
-    public void main() {
+    public void parserServiceImplHelper() {
         ProductDTO.ProductDTOBuilder productBuilder = ProductDTO.builder();
         ProductDTO.ProductDTOBuilder testBuilder = ProductDTO.builder();
         JSONObject testJson = new JSONObject();
-        float a = (float) 1578.99;
-        productBuilder.oldPrice(a);
-        a = (float) 699.0;
-        productBuilder.newPrice(a);
-        //productBuilder.link("https://lenta.com/product/vino-les-coudriers-kot-dyu-ron-lekudr-aos-gn-kr-suh-franciya-075l-233824/");
-        productBuilder.image("https://lenta.gcdn.co/globalassets/1/-/23/38/24/109234.png?preset=thumbnail");
-        productBuilder.manufacturer("Стеклянная бутылка");
-        productBuilder.brand("LES COUDRIERS");
-        productBuilder.country("Франция");
-        a = (float) 0.75;
-        productBuilder.capacity(a);
-        a = (float) 13.0;
-        productBuilder.strength(a);
-        //productBuilder.color("Красный");
-        //productBuilder.sugar("Сухое");
-        String str = "Гренаш,сира,сенсо,мурведр";
-        productBuilder.grapeSort(Arrays.asList("Гренаш,сира,сенсо,мурведр".split(", ")));
-        productBuilder.gastronomy("Отличное дополнение к блюдам из мяса, дичи, сырам.");
-        productBuilder.taste("Приятный, хорошо сбалансированный вкус.");
-        productBuilder.flavor("Приятный аромат с нотками красных фруктов.");
-        a = (float) 5.0;
-        productBuilder.rating(a);
-        productBuilder.build();
-        testJson.put("code", "458545");
-        a = (float) 5.0;
-        testJson.put("wineRating",a);
-       // testJson.put("wineLink","https://lenta.com/product/vino-les-coudriers-kot-dyu-ron-lekudr-aos-gn-kr-suh-franciya-075l-233824/");
-        testJson.put("winePackagingType","Стеклянная бутылка");
-        a = (float) 1578.99;
-        testJson.put("wineOldPrice",a);
-        a = (float) 13.0;
-        testJson.put("wineStrength",a);
-        testJson.put("wineGastronomy","Отличное дополнение к блюдам из мяса, дичи, сырам.");
-        testJson.put("wineTaste","Приятный, хорошо сбалансированный вкус.");
-        testJson.put("wineCountry","Франция");
-        testJson.put("wineAroma","Приятный аромат с нотками красных фруктов.");
-        //testJson.put("wineSugarContent","Сухое");
-        testJson.put("wineGrapeSort","Гренаш,сира,сенсо,мурведр");
-        testJson.put("imageUrl","https://lenta.gcdn.co/globalassets/1/-/23/38/24/109234.png?preset=thumbnail");
-        testJson.put("wineBrand","LES COUDRIERS");
-        //testJson.put("wineColour","Красный");
-        a = (float) 699.0;
-        testJson.put("wineNewPrice",a);
-        testJson.put("wineCapacity","0.75");
+
+        productBuilder.oldPrice(1578.99f)
+                .newPrice(699.0f)
+                .image("https://lenta.gcdn.co/globalassets/1/-/23/38/24/109234.png?preset=thumbnail")
+                .manufacturer("Стеклянная бутылка")
+                .brand("LES COUDRIERS")
+                .country("Франция")
+                .capacity(0.75f)
+                .strength(13.0f)
+                .grapeSort(Arrays.asList("Гренаш,сира,сенсо,мурведр".split(", ")))
+                .gastronomy("Отличное дополнение к блюдам из мяса, дичи, сырам.")
+                .taste("Приятный, хорошо сбалансированный вкус.")
+                .flavor("Приятный аромат с нотками красных фруктов.")
+                .rating(5.0f)
+                .build();
+
+        testJson.put("code", "458545")
+                .put("wineRating",5.0f)
+                .put("winePackagingType","Стеклянная бутылка")
+                .put("wineOldPrice",1578.99f)
+                .put("wineStrength", 13.0f)
+                .put("wineGastronomy","Отличное дополнение к блюдам из мяса, дичи, сырам.")
+                .put("wineTaste","Приятный, хорошо сбалансированный вкус.")
+                .put("wineCountry","Франция")
+                .put("wineAroma","Приятный аромат с нотками красных фруктов.")
+                .put("wineGrapeSort","Гренаш,сира,сенсо,мурведр")
+                .put("imageUrl","https://lenta.gcdn.co/globalassets/1/-/23/38/24/109234.png?preset=thumbnail")
+                .put("wineBrand","LES COUDRIERS")
+                .put("wineNewPrice",699.0f)
+                .put("wineCapacity","0.75");
+
         LentaWineParserServiceImplHelper.fillOldPrice(testJson, testBuilder);
         LentaWineParserServiceImplHelper.fillNewPrice(testJson, testBuilder);
         LentaWineParserServiceImplHelper.fillImageUrl(testJson, testBuilder);
@@ -80,6 +67,6 @@ public class LentaWineParserServiceImplHelperTest {
         LentaWineParserServiceImplHelper.fillWineGrapeSort(testJson, testBuilder);
         LentaWineParserServiceImplHelper.fillWineCapacity(testJson, testBuilder);
         testBuilder.build();
-        assertEquals( testBuilder.toString(), productBuilder.toString());
+        assertEquals(testBuilder.toString(), productBuilder.toString());
     }
 }
