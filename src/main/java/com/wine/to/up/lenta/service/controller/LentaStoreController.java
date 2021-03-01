a3c5f7ebe0c0579835f95103999bc4fb5f068822
ilyailon <ilya.ilon@mail.ru>
2021-03-02 00:26:05 +0300

    add java doc

--- src/main/java/com/wine/to/up/lenta/service/controller/LentaStoreController.java
+++ src/main/java/com/wine/to/up/lenta/service/controller/LentaStoreController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+
 /**
  * This Lenta controller class that start parsing
  */
@@ -29,6 +30,7 @@ public class LentaStoreController {
 
     /**
      * This controller starting parsing and return parsed wines
+     *
      * @return List of parsed wines
      */
     @GetMapping(name = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
