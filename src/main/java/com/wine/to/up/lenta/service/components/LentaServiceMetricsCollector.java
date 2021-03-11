2f72e5161f7356003713614e14d996f346513e25
kosch71 <kostya.chikishev@inbox.ru>
2021-03-11 17:51:49 +0300

    ## Lenta parser changes - Changing ms to seconds in metrics - Refactoring description for cron.expression

--- src/main/java/com/wine/to/up/lenta/service/components/LentaServiceMetricsCollector.java
+++ src/main/java/com/wine/to/up/lenta/service/components/LentaServiceMetricsCollector.java
@@ -100,27 +100,27 @@ public class LentaServiceMetricsCollector extends CommonMetricsCollector {
     }
 
     public void timeWinePageFetchingDuration(long time) {
-        long milliTime = TimeUnit.NANOSECONDS.toMillis(time);
+        long milliTime = TimeUnit.NANOSECONDS.toSeconds(time);
         prometheusWinePageFetchingDurationSummary.observe(milliTime);
         Metrics.summary(WINE_PAGE_FETCHING_DURATION).record(milliTime);
     }
 
     public void parsingPageDuration(long time){
-        long milliTime = TimeUnit.NANOSECONDS.toMillis(time);
+        long milliTime = TimeUnit.NANOSECONDS.toSeconds(time);
         winePageParsingDurationSummary.observe(milliTime);
         Metrics.summary(WINE_PAGE_PARSING_DURATION_SUMMARY).record(milliTime);
 
     }
 
     public void fetchingDetailsDuration(long time) {
-        long milliTime = TimeUnit.NANOSECONDS.toMillis(time);
+        long milliTime = TimeUnit.NANOSECONDS.toSeconds(time);
         wineDetailsFetchingDurationSummary.observe(milliTime);
         Metrics.summary(WINE_DETAILS_FETCHING_DURATION_SUMMARY).record(milliTime);
 
     }
 
     public void parsingDetailsDuration(long time){
-        long milliTime = TimeUnit.NANOSECONDS.toMillis(time);
+        long milliTime = TimeUnit.NANOSECONDS.toSeconds(time);
         wineDetailsParsingDurationSummary.observe(milliTime);
         Metrics.summary(WINE_DETAILS_PARSING_DURATION_SUMMARY).record(time);
     }
